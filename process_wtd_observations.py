3ef81cf5fd7a5a4072ffb55ecc10899e75a2681f
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2026-01-08 11:29:09 -0500

    Update process_wtd_observations.py

--- process_wtd_observations.py
+++ process_wtd_observations.py
@@ -6,7 +6,7 @@
 # The World Bank Group and is licensed under the terms of the MIT license.
 #
 # For inquiries, contact: info@geostack.ca
-# Repository: https://github.com/geo-stack/sahel
+# Repository: https://github.com/geo-stack/hydrodepthml
 # =============================================================================
 
 # ---- Standard imports
@@ -19,7 +19,6 @@ import geopandas as gpd
 # ---- Local imports
 from hdml import __datadir__ as datadir
 from hdml.wtd_helpers import create_wtd_obs_dataset
-from hdml.tiling import generate_tiles_bbox, filter_tiles
 
 
 def recharge_period_from_basin_area(area_km2: float) -> int:
@@ -57,7 +56,7 @@ def recharge_period_from_basin_area(area_km2: float) -> int:
     return 360
 
 
-gwl_gdf = create_wtd_obs_dataset()
+gwl_gdf = create_wtd_obs_dataset(datadir=datadir / 'wtd')
 
 # Join information about sub-basin level 12 from the HydroATLAS database.
 basins_path = datadir / 'basins' / 'basins_lvl12_102022.gpkg'
@@ -106,44 +105,11 @@ print(f"Removed {removed_count} points (from {original_count}) that "
 print(f'Final dataset has {len(gwl_gdf)} points.')
 
 # Save the water table observations dataset.
-gwl_gdf.to_file(datadir / "data" / "wtd_obs_all.gpkg", driver="GPKG")
+gwl_gdf.to_file(datadir / "wtd" / "wtd_obs_all.gpkg", driver="GPKG")
 
 # Save the basins geometry (we keep only the ones with water level obs).
 basins_gdf = basins_gdf.set_index('HYBAS_ID', drop=True)
 basins_gdf = basins_gdf.loc[gwl_gdf['HYBAS_ID'].unique()]
 basins_gdf = basins_gdf['geometry']
 
-basins_gdf.to_file(datadir / "data" / "wtd_basin_geometry.gpkg", driver="GPKG")
-
-
-# %% Tiling
-
-vrt_reprojected = datadir / 'dem' / 'nasadem_102022.vrt'
-
-# Tiles for the whole African continent bbox.
-tiles_gdf_all = generate_tiles_bbox(
-    input_raster=vrt_reprojected,
-    tile_size=5000,    # in pixels
-    overlap=100 * 30,  # 100 pixels at 30 meters resolution
-    )
-tiles_gdf_all.to_file(
-    datadir / "topo" / "tiles_geom_all.gpkg",
-    driver="GPKG")
-
-# Tiles clipped to the African continent geometry.
-tiles_gdf_africa = filter_tiles(
-    datadir / 'coastline' / 'africa_landmass.gpkg',
-    tiles_gdf_all
-    )
-tiles_gdf_africa.to_file(
-    datadir / "topo" / "tiles_geom_africa.gpkg",
-    driver="GPKG")
-
-# Tiles that contains water level observations.
-tiles_gdf = filter_tiles(
-    datadir / 'data' / 'wtd_obs_all.gpkg',
-    tiles_gdf_all
-    )
-tiles_gdf.to_file(
-    datadir / "topo" / "tiles_geom_training.gpkg",
-    driver="GPKG")
+basins_gdf.to_file(datadir / "wtd" / "wtd_basin_geometry.gpkg", driver="GPKG")
